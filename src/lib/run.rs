66f5bae55595baa7ab4e7b9af8e06f7cf2b62c9b
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2024-08-29 20:08:19 -0400

    Implement basic image argument handling in run mode

--- src/lib/run.rs
+++ src/lib/run.rs
@@ -2,6 +2,14 @@
 // created by J. Blackburn - Aug 27 2024
 //
 
+// run function should get the second argument (the image argument) 
+// and print an error message if it is not specified, otherwise continue
+
 pub fn run(args: Vec<String>) {
 
+    let image_argument: Option<&str> = args.get(2).map(String::as_str);
+
+    let image_path: &str = image_argument.expect("Error: no image path specified"); // TODO: improve error message
+
+    println!("Image path exists, continuing in run mode with image: {}", image_path);
 }
