842ae758626bf9955679f254b22b59dd09a980e3
Jake Blackburn <jablackburn@wm.edu>
2024-10-03 13:13:35 -0400

    Add basic menu options functionality

--- src/lib/view.rs
+++ src/lib/view.rs
@@ -17,6 +17,7 @@ use crate::lib::{filters, panel};
 use eframe::egui;
 use image::{ImageBuffer, Rgba};
 
+use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::io::{stdin, stdout, Write};
 use std::thread;
@@ -32,12 +33,16 @@ pub fn view(image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>>) {
 
         // create update switch
     let update_switch: SharedBoolean = Arc::new(Mutex::new(true));
-    
-    
 
         // do stuff with image buffer in separate thread
+    let background_image_buffer = shareable_image_buffer.clone();
     let background_update_switch = update_switch.clone();
     let background_thread = thread::spawn(move || { // TODO: move thread to separate function
+
+            // TODO:
+        // add shared quit signal
+
+
 
             // initialize menu option enum
         #[derive(PartialEq)]
@@ -75,7 +80,7 @@ pub fn view(image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>>) {
             let _ = stdout().flush();
 
                 // match against input for MenuOptions enum variants
-            menu_option = match String::as_str(&input) {
+            menu_option = match input.trim() {
                 "f" => MenuOption::Filter,
                 "r" => MenuOption::Reset,
                 "s" => MenuOption::Save,
@@ -90,21 +95,28 @@ pub fn view(image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>>) {
                     continue;
                 }
 
-
                 MenuOption::Save  => {
-                    println!("Saving image");
+                    print!("Saving image - Specify output path:");
+                    let _ = stdout().flush();
 
-                    // get / create output path
-                    // save to output path
+                        // get / create output path
+                    let mut output_path = String::new();
+                    stdin().read_line(&mut output_path).expect("Error: could not read user input");
+                    let _ = stdout().flush();
+
+                        // save to output path
+                    let locked_image_buffer = background_image_buffer.lock().unwrap();
+                    locked_image_buffer.save(Path::new(output_path.trim())); 
 
                     continue;
                 }
 
                 MenuOption::Reset => {
-                    println!("Reloading Image from static buffer");
+                    println!("loading original image");
+                    println!("This actually does nothing right now");
 
-                        // pass to resetting method?
-                        
+                        // pass control to resetting method?
+
                         // toggle update switch to trigger reload
                     let mut locked_update_switch = background_update_switch.lock().unwrap();
                     *locked_update_switch = true; 
@@ -122,12 +134,20 @@ pub fn view(image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>>) {
                         //
                         // pass to filter handler method?
                     
-                        // toggle update switch to trigget reload
+                    print!("Enter filter name: ");
+                    let _ = stdout().flush();
+
+                    let mut filter_name = String::new();
+                    stdin().read_line(&mut filter_name).expect("Error: could not read user input");
+                    let _ = stdout().flush();
+
+                    // filter::filter(filter_name, backround_image_buffer.clone())
+
+                        // toggle update switch to trigger reload
                     let mut locked_update_switch = background_update_switch.lock().unwrap();
                     *locked_update_switch = true; 
 
                     continue;
-                        
                 }
 
                 MenuOption::Quit  => { continue; }
