e6524c4362d44c16341468c6746912b227b9d15e
Jake Blackburn <jablackburn@wm.edu>
2024-10-03 12:45:35 -0400

    Move load image buffer to separate function

--- src/main.rs
+++ src/main.rs
@@ -37,27 +37,15 @@ enum Mode {
 
 fn main() {
 
+        // get cli argument vector
     let args: Vec<String> = env::args().collect();
 
-    // turn 1st argument into Mode enum variant
+        // turn first argument into Mode enum variant
     let mode_argument = match args.get(1).map(String::as_str) {
         Some("view") => Mode::View,
         Some("run")  => Mode::Run,
         _            => Mode::Invalid
     };
-
-
-    let image_path: &str = args
-                                    .get(2)
-                                    .map(String::as_str)
-                                    .expect("Error: no image path specified"); 
-
-
-    println!("Attempting to load input image: {}", image_path); 
-    let image_buffer = image::open(image_path)
-                        .expect("Error: run mode couldnt open your image") 
-                        .to_rgba();
-
 
     match mode_argument {
 
@@ -75,8 +63,26 @@ fn main() {
             // TODO:
                 // find a way to not repeat view/run three times in a row
 
-        Mode::View => { View::view(image_buffer) }
+        Mode::View => { View::view(load_image_buffer(&args)) }
 
-        Mode::Run  => { Run::run(image_buffer, args) }
+        Mode::Run  => { Run::run(load_image_buffer(&args), args) }
     }
 }
+
+
+
+fn load_image_buffer(args: &Vec<String>) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    
+    let image_path: &str = args
+                            .get(2)
+                            .map(String::as_str)
+                            .expect("Error: no image path specified"); 
+
+    println!("Attempting to load input image: {}", image_path); 
+
+        // return buffer
+    image::open(image_path)
+            .expect("Error: run mode couldnt open your image") 
+            .to_rgba()
+}
+
